d63fa07ba1b5a6d2a8397a17bff2197d2f29163b
MrLimeick <apmew8899@gmail.com>
2026-01-06 16:11:10 +0300

    06.01.26

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.tab').forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        document.querySelectorAll('.tab.active').forEach(activeTab => {
+            activeTab.classList.remove('active');
+        });
+
+        e.currentTarget.classList.add('active');
+    });
+});
+
+let nav = this.document.getElementById("navbar")
+let logo = this.document.getElementById("logo")
+
+window.addEventListener("scroll", function (e) {
+    nav.classList = this.window.scrollY >= 450 ? "frame-3 fixed" : "frame-3"
+})
